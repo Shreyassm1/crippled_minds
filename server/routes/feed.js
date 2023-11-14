169dd98650838206d8c57f58f28c33f0d7db9f86
Shreyas Mishra <111569029+Shreyassm1@users.noreply.github.com>
2023-11-14 11:01:52 +0530

    Add files via upload

---
+++ server/routes/feed.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const 
+const User = require("./models/User")
+const isAuthenticated = require("./server");
+
+//create post
+router.post('/upload', async(req,res) => {
+    
+    const 
+
+})
