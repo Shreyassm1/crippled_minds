1e9ff400af5d125e9567f44ff731fc204f7a453b
Shreyas Mishra <111569029+Shreyassm1@users.noreply.github.com>
2023-11-20 17:03:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
+const port = process.env.PORT || 8000;
+const server = app.listen(port,()=> console.log(`server is running ${port}`));
+const fs = require("fs");
 const express = require("express");
 const mongoose = require("mongoose");
+const LocalStrategy = require('passport-local').Strategy; 
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const session = require("express-session");
 const cors = require("cors");
 const path = require("path");
 const app = express();
+const User = require("./models/User");
+const auth = require('./routes/auth')
 app.use(bodyParser.json());
 app.use(cors());
-app.use(session({secret : 'fstiwrhsb', resave : false, saveUninitialized : false}));
+app.use(session({ secret: 'fstiwrhsb', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use('/mnnitchessclub', auth);
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -23,6 +30,7 @@ const isAuthenticated = (req, res, next) => {
   res.status(401).json({ error: "Not authenticated" });
 };
 
+fs.readdirSync('./routes').map((r)=> app.use(require('./routes/' + r)));
 mongoose
   .connect("mongodb+srv://altshreyas:fstiwrhsb@cluster0.kjs4euw.mongodb.net/?retryWrites=true&w=majority",{
   })
