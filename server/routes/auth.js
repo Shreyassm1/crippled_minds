5f5f7d8ccb205cd8b219bb4c32fbd28360906830
Shreyas Mishra <111569029+Shreyassm1@users.noreply.github.com>
2023-11-20 17:10:06 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,37 +2,53 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const isAuthenticated = require("./server");
+const isAuthenticated = require("../server");
 
+router.post('/register', async (req, res) => {
+    console.log("Route accessed");
 
-router.post("/register", async(res,req) => {
-    
-    //Discrepancies
-    const {firstName, lastName, email, password} = req.body;
-    if (!firstName || !lastName || !email) {
-        return res.status(400).json({error : "Invalid request body"});
+    try {
+        const { username, email, password } = req.body;
+        console.log("Request Body:", req.body);
+
+        if (!username || !email || !password) {
+            console.log("Invalid request body");
+            return res.status(400).json({ error: "Invalid request body" });
+        }
+
+        const existingUserByEmail = await User.findOne({ email: email.trim() });
+        const existingUserByUsername = await User.findOne({ username: username.trim() });
+
+        if (existingUserByEmail || existingUserByUsername){
+            console.log("Existing User:", existingUser);
+        }
+
+        if (existingUserByEmail || existingUserByUsername) {
+            console.log("User with email already exists");
+            return res.status(409).json({ error: "A user with this email already exists" });
+        }
+        console.log("Creating a new user");
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUserDetails = {
+            username,
+            email,
+            password: hashedPassword,
+        };
+
+        const newUser = await User.create(newUserDetails);
+        console.log("New User Created:", newUser);
+
+        return res.status(200).json({ message: "User created successfully" });
+        
+    } catch (error) {
+        console.error("Error in route:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
     }
-
-    const existingUser = await UserActivation.findOne({email : email});
-    if (existingUser){
-        return res
-            .status(402)
-            .json({error : "A user with this email already exists"}); 
-    }
-
-    //valid
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUserDetails = {
-        firstName, 
-        lastName, 
-        email, 
-        password : hashedPassword,
-    };
-    const newUser = await User.create(newUserDetails);
-
 });
 
-router.post("/login", isAuthenticated, async(req,res) => {
+
+router.post("/login", async(req,res) => {
 
     //Discrepancies
     const {email, password} = req.body;
@@ -52,7 +68,7 @@ router.post("/login", isAuthenticated, async(req,res) => {
 
     //valid
     return res.status(200).json({message : "Login Successfull"});
-
 });
 
-module.exports = router();
+module.exports = router;
+
