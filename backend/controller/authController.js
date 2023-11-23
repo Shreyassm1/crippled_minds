3bc8278f13251ac9c959150257a57ba4f4f37fdf
Shreyas Mishra <111569029+Shreyassm1@users.noreply.github.com>
2023-11-24 01:58:10 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,10 +2,18 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8000';
 
+export const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
 export const registerUser = async (registrationData) => {
   try {
     const response = await axios.post(`${BASE_URL}/register`, registrationData);
-
+    
     return { success: true, data: response.data };
   } catch (error) {
     console.error('Error during user registration:', error);
@@ -16,7 +24,9 @@ export const registerUser = async (registrationData) => {
 export const loginUser = async (loginData) => {
   try {
     const response = await axios.post(`${BASE_URL}/login`, loginData);
-
+    console.log("response:", response.data);
+    const { token } = response.data;
+    setAuthToken(token);
     return { success: true, data: response.data };
   } catch (error) {
     console.error('Error during user Login:', error);
